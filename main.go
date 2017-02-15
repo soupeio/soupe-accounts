a876e412fc4b4d331d34cc81673447f1e1dea239
hukl <contact@smyck.org>
2017-02-15 21:10:15 +0100

    decided to use ordinary session ids
    experimented with jwtokens but after reading some guides and papers it
    seems they don't actually add any value for the auth flow

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"net/http"
 
 	"gopkg.in/gin-gonic/gin.v1"
@@ -13,17 +15,37 @@ type Register struct {
 	PasswordConfirmation string `form:"password_confirmation" binding:"required"`
 }
 
+func GenerateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	// Note that err == nil only if we read len(b) bytes.
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func GenerateRandomString(s int) (string, error) {
+	b, err := GenerateRandomBytes(s)
+	return base64.URLEncoding.EncodeToString(b), err
+}
+
 func main() {
 	// Creates a gin router with default middleware:
 	// logger and recovery (crash-free) middleware
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 
+	router.Static("/assets", "./assets")
+
 	router.GET("/register", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "register.tmpl", gin.H{})
+		cookie, _ := c.Cookie("id")
+		c.HTML(http.StatusOK, "register.tmpl", gin.H{"cookie": cookie})
 	})
 
 	router.POST("/register", func(c *gin.Context) {
+
 		var form Register
 		if c.Bind(&form) == nil {
 			c.HTML(http.StatusOK, "register_success.tmpl", gin.H{
@@ -34,6 +56,20 @@ func main() {
 		}
 
 	})
+
+	router.GET("/get_token", func(c *gin.Context) {
+		random_bytes, _ := GenerateRandomString(128)
+
+		c.SetCookie("id", random_bytes, 3600, "/", "", false, false)
+
+		c.HTML(http.StatusOK, "get_token.tmpl", gin.H{
+			"token_string": "Hallo",
+			"token":        "Ballo",
+			"claims":       random_bytes,
+		})
+
+	})
+
 	// By default it serves on :8080 unless a
 	// PORT environment variable was defined.
 	router.Run()
